78aa9fbb2791925052009e85f96df32140fcad6f
melki Balbino <melkibalbinoads@gmail.com>
2019-03-03 23:59:06 -0300

    Foram Criados as Classes Avaliação, Pergunta e Resposta no models.py

--- avaliacao_perfil/models.py
+++ avaliacao_perfil/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Avaliacao(models.Model):
+    titulo = models.CharField(max_length=350, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return '%d - %s' %(self.id, self.titulo)
+
+class Pergunta(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+    avaliacao_id = models.ForeignKey(to=Avaliacao, on_delete=models.CASCADE)
+
+class Resposta(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+    pergunta_id = models.ForeignKey(to=Pergunta, on_delete=models.CASCADE)
