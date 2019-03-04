d1af0c60374db7fd334dd5f8a752644183ed4027
melki Balbino <melkibalbinoads@gmail.com>
2019-03-04 06:23:43 -0300

    Feito modigicações no models.py, admin.py, como tambem, foi preenchido o banco de dados

--- avaliacao_perfil/admin.py
+++ avaliacao_perfil/admin.py
@@ -1,6 +1,63 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Avaliacao)
-admin.site.register(Pergunta)
-admin.site.register(Resposta)
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'resumo')
+    search_fields = ('titulo',)
+
+    def resumo(self, obj):
+        desc = obj.descricao
+        if desc:
+            if len(desc) > 40:
+                return '%s...' % desc[:35]
+        return desc
+
+@admin.register(Pergunta)
+class PerguntaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'avaliacao',)
+
+    list_filter = ('avaliacao',)
+    search_fields = ('nome',)
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'tipo_perfil', 'resumo', 'padrao_comportamento',)
+    search_fields = ('nome', 'tipo_perfil', 'padrao_comportamento',)
+
+    def resumo(self, obj):
+        desc = obj.descricao
+        if desc:
+            if len(desc) > 40:
+                return '%s...' % desc[:35]
+        return desc
+
+@admin.register(Resposta)
+class RespostaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'pergunta', 'perfil',)
+    list_filter = ('perfil', 'pergunta',)
+    search_fields = ('nome',)
+
+@admin.register(Comportamento)
+class ComportamentoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'perfil',)
+    list_filter = ('perfil',)
+    search_fields = ('nome',)
+
+@admin.register(PontoForte)
+class PontoForteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'perfil',)
+    list_filter = ('perfil',)
+    search_fields = ('nome',)
+
+@admin.register(PontoDaMelhoria)
+class PontoDaMelhoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'perfil',)
+    list_filter = ('perfil',)
+    search_fields = ('nome',)
+
+@admin.register(Motivacao)
+class MotivacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'perfil',)
+    list_filter = ('perfil',)
+    search_fields = ('nome',)
--- avaliacao_perfil/models.py
+++ avaliacao_perfil/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Avaliacao(models.Model):
     titulo = models.CharField(max_length=350, unique=True)
     descricao = models.TextField(blank=True, null=True)
@@ -7,54 +8,58 @@ class Avaliacao(models.Model):
     def __str__(self):
         return self.titulo
 
+
 class Pergunta(models.Model):
     nome = models.CharField(max_length=350, unique=True)
-    avaliacao_id = models.ForeignKey(to=Avaliacao, on_delete=models.CASCADE)
+    avaliacao = models.ForeignKey(to=Avaliacao, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%d - %s' %(self.id, self.nome)
+
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+    tipo_perfil = models.CharField(max_length=350, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+    imagem_perfil = models.ImageField(upload_to='media/img')
+    padrao_comportamento = models.CharField(max_length=450)
 
     def __str__(self):
         return self.nome
 
+
 class Comportamento(models.Model):
     nome = models.CharField(max_length=350, unique=True)
+    perfil = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
 
 class PontoForte(models.Model):
     nome = models.CharField(max_length=350, unique=True)
+    perfil = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
 
 class PontoDaMelhoria(models.Model):
     nome = models.CharField(max_length=350, unique=True)
+    perfil = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
 
 class Motivacao(models.Model):
     nome = models.CharField(max_length=350, unique=True)
-
-    def __str__(self):
-        return self.nome
-
-class Perfil(models.Model):
-    nome = models.CharField(max_length=350, unique=True)
-    descricao = models.TextField(blank=True, null=True)
-    imagem_perfil = models.ImageField(upload_to='media/img')
-    padrao_comportamento = models.CharField(max_length=450)
-    comportamento_id = models.ForeignKey(to=Comportamento, on_delete=models.CASCADE)
-    ponto_forte_id = models.ForeignKey(to=PontoForte, on_delete=models.CASCADE)
-    ponto_da_melhoria_id = models.ForeignKey(to=PontoDaMelhoria, on_delete=models.CASCADE)
-    motivacao_id = models.ForeignKey(to=Motivacao, on_delete=models.CASCADE)
+    perfil = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
 
 class Resposta(models.Model):
     nome = models.CharField(max_length=350, unique=True)
-    pergunta_id = models.ForeignKey(to=Pergunta, on_delete=models.CASCADE)
-    perfil_id = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
+    pergunta = models.ForeignKey(to=Pergunta, on_delete=models.CASCADE)
+    perfil = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
