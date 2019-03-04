e5f9dac6f5d38c1c5d35477fde4246b275569109
melki Balbino <melkibalbinoads@gmail.com>
2019-03-04 00:57:20 -0300

    Foram Criados as classes restantes no arquivo models.py

--- avaliacao_perfil/models.py
+++ avaliacao_perfil/models.py
@@ -5,12 +5,56 @@ class Avaliacao(models.Model):
     descricao = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return '%d - %s' %(self.id, self.titulo)
+        return self.titulo
 
 class Pergunta(models.Model):
     nome = models.CharField(max_length=350, unique=True)
     avaliacao_id = models.ForeignKey(to=Avaliacao, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nome
+
+class Comportamento(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class PontoForte(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class PontoDaMelhoria(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Motivacao(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=350, unique=True)
+    descricao = models.TextField(blank=True, null=True)
+    imagem_perfil = models.ImageField(upload_to='media/img')
+    padrao_comportamento = models.CharField(max_length=450)
+    comportamento_id = models.ForeignKey(to=Comportamento, on_delete=models.CASCADE)
+    ponto_forte_id = models.ForeignKey(to=PontoForte, on_delete=models.CASCADE)
+    ponto_da_melhoria_id = models.ForeignKey(to=PontoDaMelhoria, on_delete=models.CASCADE)
+    motivacao_id = models.ForeignKey(to=Motivacao, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
 class Resposta(models.Model):
     nome = models.CharField(max_length=350, unique=True)
     pergunta_id = models.ForeignKey(to=Pergunta, on_delete=models.CASCADE)
+    perfil_id = models.ForeignKey(to=Perfil, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
